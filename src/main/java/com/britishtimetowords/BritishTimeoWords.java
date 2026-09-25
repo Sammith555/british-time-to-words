1bf5d93e68e1865350acd6a543e577a2e79fe35b
SAMMIBS <sammith.b_s@mercedes-benz.com>
2026-09-25 21:32:21 +0530

    Rename Main to BritishTimeToWords, add resources/test dirs, enable gradlew

---
+++ src/main/java/com/britishtimetowords/BritishTimeoWords.java
@@ -0,0 +1,17 @@
+package com.britishtimetowords;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BritishTimeoWords implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(BritishTimeoWords.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello, world!");
+    }
+}
